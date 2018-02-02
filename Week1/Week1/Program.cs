3aedb800347e24a71d9f73f99c72cbddc2f848b1
Akash Kansara <skyline_96@live.com>
2018-02-02 17:02:05 +0530

    Initial Commit

---
+++ Week1/Week1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    class Employee  {
+        private int employeeno;
+        private string firstname;
+        private string lastname;
+        private string address;
+        private string city;
+        private string state;
+        private int postalcode;
+
+
+        public int EmployeeNo { get { return employeeno; } set { employeeno = value; } }
+        public string FirstName { get { return firstname; } set { firstname = value; } }
+        public string LastName { get { return lastname; } set { lastname = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string City { get { return city; } set { city = value; } }
+        public string State { get { return state; } set { state = value; } }
+        public int PstalCode { get { return postalcode; } set { postalcode = value; } }
+    }
+
+    class Product   {
+        private int productno;
+        private string productname;
+        private int unitprice;
+        private bool isactive;
+
+
+        public int ProductNo { get { return productno; } set { productno = value; } }
+        public string ProductName { get { return productname; } set {productname = value; } }
+        public int UnitPrice { get { return unitprice; } set { unitprice = value; } }
+        public bool IsActive { get { return isactive; } set { isactive = value; } }
+    }
+
+    class Customer  {
+        private int customerno;
+        private string customername;
+        private string address;
+        private string city;
+        private string state;
+        private int postalcode;
+        private string country;
+
+
+        public int CustomerNo { get { return customerno; } set { customerno = value; } }
+        public string CustomerName { get { return customername; } set { customername = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string City { get { return city; } set { city = value; } }
+        public string State { get { return state; } set { state = value; } }
+        public int PstalCode { get { return postalcode; } set { postalcode = value; } }
+    }
+    
+    class Order {
+        private int orderno;
+        private Customer customerdetail;
+        private Employee employeedetail;
+        private DateTime orderdate;
+        private string shipname;
+        private string shipaddress;
+        private string shipcity;
+        private string shipstate;
+        private int shippostalcode;
+        private string shipcountry;
+        private DateTime createddate;
+        private DateTime modifieddate;
+
+
+        public int OrderNo { get { return orderno; } set { orderno = value;} }
+        public Customer CustomerDetail { get { return customerdetail; } set { customerdetail = value; } }
+        public Employee EmployeeDetail { get { return employeedetail; } set { employeedetail = value; } }
+        public DateTime OrderDate { get { return orderdate; } set { orderdate = value; } }
+        public string ShipName { get { return shipname; } set { shipname = value; } }
+        public string ShipCity { get { return shipcity; } set { shipcity = value; } }
+        public string ShipState { get { return shipstate; } set { shipstate = value; } }
+        public int ShipPostalCode { get { return shippostalcode; } set { shippostalcode = value; } }
+        public string ShipCountry { get { return shipcountry; } set { shipcountry = value; } }
+        public DateTime CreatedDate { get { return createddate; } set { createddate = value; } }
+        public DateTime ModifiedDate { get { return modifieddate; } set { modifieddate = value; } }
+    }
+
+    class OrderDetail
+    {
+        private Product[] productdetail;
+        private int unitprice;
+        private int quantity;
+        private int amount;
+        private int discountamount;
+        private int grandtotal;
+        private DateTime createddate;
+        private DateTime modifieddate;
+
+        public Product[] ProProductDetail { get { return productdetail; } set { productdetail = value; } }
+        public int UnitPrice { get { return unitprice; } set { unitprice = value; } }
+        public int Quantity { get { return quantity; } set { quantity = value; } }
+        public int Amount { get { return amount; } set { amount = value; } }
+        public int DiscountAmount { get { return discountamount; } set { discountamount = value; } }
+        public int GrandTotal { get { return grandtotal; } set { grandtotal = value; } }
+        public DateTime CreatedDate { get { return createddate; } set { createddate = value; } }
+        public DateTime ModifiedDate { get { return modifieddate; } set { modifieddate = value; } }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Add Order");
+            Console.WriteLine("2. Update Order");
+            Console.WriteLine("3. Delete Order");
+
+            int input = Convert.ToInt32(Console.ReadKey());
+
+            switch (input)
+            {
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            
+        }
+    }
+}
